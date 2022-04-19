3f221725fa08f66987b4344dc8a6133783e216e0
icep0ps <tapsmuko@gmail.com>
2022-04-19 20:33:50 +0200

    link fix

--- main.js
+++ main.js
@@ -5,16 +5,12 @@ const episode = document.querySelector('#episode')
 const score = document.querySelector('#score')
 const stat = document.querySelector('#status')
 
-
-
-
 let myLibray = []
-
 
 function Anime(title, episode, score, status) {
 
     this.title = title
-    this.episode =episode
+    this.episode = episode
     this.score = score
     this.status = status
 
@@ -24,44 +20,37 @@ Anime.prototype.show = function () {
     return (this.title + this.episode)
 }
 
-
 function addAnimeToLibrary() {
-
-    let anime = new Anime(title.value,episode.value, score.value, stat.value)
+    let anime = new Anime(title.value, episode.value, score.value, stat.value)
     myLibray.push(anime.title)
     createAnime(anime)
 
-
 }
-
-
 
 function createAnime(anime) {
 
-            const btn = document.createElement('button')
-            const btnTd = document.createElement('td')
-            const row = document.createElement('tr')
-            const title = document.createElement('td')
-            const episode = document.createElement('td')
-            const score = document.createElement('td')
-            const status = document.createElement('td')
-            table.append(row)
-            btnTd.append(btn)
-            row.append(title,episode, score, status, btnTd)
-            title.textContent = anime.title
-            episode.textContent = anime.episode
-            score.textContent = anime.score
-            status.textContent = anime.status
-            btn.textContent = 'Delete'
-            row.setAttribute('data-anime', title.textContent)
-            btn.setAttribute('data-anime', title.textContent)
-            btn.addEventListener('click', e => deleteAnime(anime.title))
-            
-//when created the button parses the anime title as an argument on that button
+    const btn = document.createElement('button')
+    const btnTd = document.createElement('td')
+    const row = document.createElement('tr')
+    const title = document.createElement('td')
+    const episode = document.createElement('td')
+    const score = document.createElement('td')
+    const status = document.createElement('td')
+    table.append(row)
+    btnTd.append(btn)
+    row.append(title, episode, score, status, btnTd)
+    title.textContent = anime.title
+    episode.textContent = anime.episode
+    score.textContent = anime.score
+    status.textContent = anime.status
+    btn.textContent = 'Delete'
+    row.setAttribute('data-anime', title.textContent)
+    btn.setAttribute('data-anime', title.textContent)
+    btn.addEventListener('click', e => deleteAnime(anime.title))
+
+    //when created the button parses the anime title as an argument on that button
 
 }
-
-
 
 function deleteAnime(title) {
     myLibray.forEach(anime => {
