513187114f2f3b464d1ac1d34ea910f90a4df3bb
icep0ps <tapsmuko@gmail.com>
2023-12-28 21:26:27 +0200

    feat: added server and sql driver :sparkles:

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/anime', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/anime', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
