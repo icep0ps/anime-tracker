7d58acdd5c1929c388bb4c516e84c2dc88462f38
icep0ps <tapsmuko@gmail.com>
2023-12-31 20:18:16 +0200

    feat: added error handinling middleware and anime route :sparkles:

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ import cors from 'cors';
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import animeRoute from './routes/anime/route.js';
 import searchRoute from './routes/search/route.js';
+import errorHandler from './middleware/errorHandler.js';
 
 const app = express();
 const port = 3000;
@@ -15,14 +17,16 @@ app.set('views', path.join(path.resolve(), 'server/views'));
 app.use(express.static(path.join(path.resolve(), '/public')));
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(errorHandler);
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.use('/search', searchRoute);
+app.use('/anime', animeRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
