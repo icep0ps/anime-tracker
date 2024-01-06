e09b0d9878e1fd945bca383a943cb091457402a9
icep0ps <tapsmuko@gmail.com>
2024-01-06 23:55:34 +0200

    chore: remove deleted files

--- server/routes/anime/route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import Anime from '../../controllers/anime.js';
-const router = express.Router();
-
-router.get('/', Anime.get);
-router.post('/', Anime.create);
-router.delete('/', Anime.delete);
-
-export default router;
