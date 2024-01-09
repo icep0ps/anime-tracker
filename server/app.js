444939d320ef563b43fc1fdc95ce1ae0bff70ca1
icep0ps <tapsmuko@gmail.com>
2024-01-09 23:28:26 +0200

    feat: config auth using passportjs :sparkles:

--- server/app.js
+++ server/app.js
@@ -2,13 +2,19 @@ import 'dotenv/config';
 import path from 'path';
 import cors from 'cors';
 import express from 'express';
+import passport from 'passport';
 import bodyParser from 'body-parser';
+import session from 'express-session';
+import LocalStrategy from 'passport-local';
 
+import Auth from './controllers/auth.js';
 import Home from './controllers/home.js';
+
 import authRoute from './routes/auth/route.js';
 import entryRoute from './routes/entry/route.js';
 import searchRoute from './routes/search/route.js';
 import errorHandler from './middleware/errorHandler.js';
+import Locals from './providers/locals.js';
 
 class App {
   static init(database) {
@@ -27,8 +33,19 @@ class App {
 
     app.use(cors());
     app.use(errorHandler);
-    app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(
+      session({
+        secret: Locals.config().secret,
+        resave: false,
+        saveUninitialized: false,
+      })
+    );
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+    passport.use(new LocalStrategy({ passReqToCallback: true }, Auth.authenticate));
 
     app.get('/', exposeDatabase, Home.load);
     app.use('/auth', exposeDatabase, authRoute);
