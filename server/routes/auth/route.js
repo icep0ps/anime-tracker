ef73f6c06ee8fbe27275358f195b394602b86c49
icep0ps <tapsmuko@gmail.com>
2024-01-11 22:54:24 +0200

    feat: add user auth middleware :sparkles:

--- server/routes/auth/route.js
+++ server/routes/auth/route.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import passport from 'passport';
 import Auth from '../../controllers/auth.js';
 import Validator from '../../middleware/validator.js';
 
@@ -7,7 +8,23 @@ const router = express.Router();
 router.get('/login', Auth.page.login);
 router.get('/signup', Auth.page.signup);
 
-router.post('/login', Validator.login, Auth.login);
+router.post('/logout', Auth.logout);
+router.post(
+  '/login',
+  Validator.login,
+  passport.authenticate(
+    'local',
+    {
+      successRedirect: '/',
+      failureRedirect: '/auth/login',
+    },
+    function (request, response) {
+      console.log(request.user.username);
+      response.redirect('/');
+    }
+  ),
+  Auth.login
+);
 router.post('/signup', Validator.signup, Auth.signup);
 
 export default router;
