2d91970d407092c19c3d19b5024912c03a9153f1
icep0ps <tapsmuko@gmail.com>
2024-05-13 17:14:00 +0200

    tests: added mock data :rotating_light:

--- tests/mocks/providers/database.js
+++ tests/mocks/providers/database.js
@@ -1,28 +1,38 @@
+import users from './data/users.json';
+import anime from './data/anime.json';
+import entries from './data/entries.json';
+
 import Entry from '../../mocks/classes/entry.js';
 
 class Database {
+  static connect() {
+    return Promise.resolve();
+  }
+
   static create = {
     async entry(status, progress, rating, notes, started, finished, userid, animeid) {},
     async anime(anime) {},
-
     async user(user) {},
   };
 
   static get = {
-    async user(username, password) {},
-
-    async entries(userid) {
-      return Promise.resolve([
-        new Entry(userid, 1, 'watching'),
-        new Entry(userid, 2, 'dropped'),
-        new Entry(userid, 3, 'watching'),
-        new Entry(userid, 4, 'paused'),
-      ]);
+    async user(username) {
+      return Promise.resolve(users.filter((user) => user.username == username)[0]);
     },
 
-    async entry(userid, entryid) {},
+    async entries(userid) {
+      return Promise.resolve(entries.filter((entry) => entry.user_id == userid));
+    },
 
-    async anime(id) {},
+    async entry(userid, entryid) {
+      return Promise.resolve(
+        entries.filter((entry) => entry.user_id == userid && entry.id == entryid)
+      );
+    },
+
+    async anime(id) {
+      return Promise.resolve(anime.filter((anime) => anime.id == id));
+    },
   };
 
   static delete = {
